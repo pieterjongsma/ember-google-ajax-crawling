28fb50d709cb163639c59016650d7687304c2922
Pieter Jongsma <pieter.jongsma@gmail.com>
2013-01-10 17:24:06 +0100

    Fresh Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ItemApp::Application.config.secret_token = '86f5ba8a6f7207b3eafe86ce1667c5a070f8380f18b5c1b853cd96718b0e08cdfd304fe87bcde12a2a5e0dfd7c3c0e4881461b9a3361e20e928ee22dbdd8da3f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ItemApp::Application.config.session_store :cookie_store, key: '_ItemApp_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ItemApp::Application.config.session_store :active_record_store
