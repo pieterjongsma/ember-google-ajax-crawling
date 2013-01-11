8a0c94c178e4b8093cb4f241e15efefb1dae8ce9
Pieter Jongsma <pieter.jongsma@gmail.com>
2013-01-11 22:33:06 +0100

    Setup escaped_fragment catching.

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.11'
 
+gem 'passenger'
+
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,15 @@ class ApplicationController < ActionController::Base
   
   def catch_escaped_fragment
     if fragment = params[:_escaped_fragment_]
-      # TODO
+      # Build the original url
+      base_url = "#{request.protocol}#{request.host_with_port}#{request.path}"
+      url_with_hash = "#{base_url}#!#{params[:_escaped_fragment_]}"
+      
+      # Render it with the PhantomJS script and return the result
+      command = "phantomjs '#{Rails.root}/lib/phantomjs/ember_to_static.js' '#{url_with_hash}' silence"
+      result = `#{command}`
+      
+      render text: result
     end
   end
 end
