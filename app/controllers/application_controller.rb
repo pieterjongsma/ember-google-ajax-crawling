0d1fa6e10479b9d26b11295b68c06753ea9732d6
Pieter Jongsma <pieter.jongsma@gmail.com>
2013-01-11 21:16:07 +0100

    Added ember_to_static.js which is a PhantomJS script.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :catch_escaped_fragment
+  
+  protected
+  
+  def catch_escaped_fragment
+    if fragment = params[:_escaped_fragment_]
+      # TODO
+    end
+  end
 end
